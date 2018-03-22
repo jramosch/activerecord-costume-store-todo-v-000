3a4498169fa0684434e4c3b9ef3344329e28fb95
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-22 00:59:22 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,11 @@
 class CreateCostumeStores < ActiveRecord::Migration
-
+  def change
+    create_table :costume_stores do |c|
+      c.string :name
+      c.string :location
+      c.integer :inventory
+      c.integer :num_of_employees
+      
+    end
+  end
 end
